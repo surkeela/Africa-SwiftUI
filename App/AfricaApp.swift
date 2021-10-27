916146a7e77987733ce46ccd6b9f7b949e43c030
Nerijus <surkeela@yahoo.com>
2021-10-27 13:14:22 +0300

    Initial commit

---
+++ App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Nerijus Surkys on 2021-10-27.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
